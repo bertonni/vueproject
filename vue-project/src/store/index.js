7c74cc9d2f3496a9b09fb0c8651705b5810e779a
bertonnipaz <bertonnipaz@gmail.com>
2021-05-09 17:51:12 -0300

    Creating the store variables to use on components

--- vue-project/src/store/index.js
+++ vue-project/src/store/index.js
@@ -1,9 +1,17 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    counter: 0,
+    username: '<Your Name>',
   },
   mutations: {
+    setCounter(state, value) {
+      state.counter += (value ?? 1);
+    },
+    setUsername(state, newName) {
+      state.username = newName;
+    }
   },
   actions: {
   },
